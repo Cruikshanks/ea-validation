8ed2d630facba7913efbe4ce88e71baa2acd61d8
Rob Nichols <git@nicholshayes.co.uk>
2016-07-20 13:06:02 +0100

    Initial gem framework

---
+++ lib/ea/validation.rb
@@ -0,0 +1,7 @@
+require "ea/validation/version"
+
+module Ea
+  module Validation
+    # Your code goes here...
+  end
+end
---
+++ lib/ea/validation/version.rb
@@ -0,0 +1,5 @@
+module Ea
+  module Validation
+    VERSION = "0.1.0"
+  end
+end
